99034c61251d0c637941835f1b1da84e467ca2ee
Pranav <pranav.p@seeroo.com>
2022-04-19 21:56:35 +0530

    [added]endpoint user

---
+++ src/main/java/com/seeroo/rentalservice/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.seeroo.rentalservice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @PostMapping (value = "/user")
+    public String user(@RequestBody UserDetailsDTO userDetailsDTO){
+        return "My name is " + userDetailsDTO.getName() + "\n" +
+               "I am from " +userDetailsDTO.getLocation() + "\n" +
+               "I am working at " + userDetailsDTO.getCompany() ;
+    }
+
+
+}
---
+++ src/main/java/com/seeroo/rentalservice/dto/UserDetailsDTO.java
@@ -0,0 +1,31 @@
+package com.seeroo.rentalservice.controller;
+
+public class UserDetailsDTO {
+    private String name;
+    private String company;
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
