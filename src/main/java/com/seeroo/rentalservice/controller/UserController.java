6cb6e63bd726942714c6111e287cf5f4976dcc94
Pranav <pranav.p@seeroo.com>
2022-04-21 01:25:25 +0530

    [added]if-else statement

--- src/main/java/com/seeroo/rentalservice/controller/UserController.java
+++ src/main/java/com/seeroo/rentalservice/controller/UserController.java
@@ -9,12 +9,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @PostMapping (value = "/user")
-    public String user(@RequestBody UserDetailsDTO userDetailsDTO){
-        return "My name is " + userDetailsDTO.getName() + "\n" +
-               "I am from " +userDetailsDTO.getLocation() + "\n" +
-               "I am working at " + userDetailsDTO.getCompany() ;
+    public static String country = "India";
+    public static String state = "Kerala";
+    public static String company = "Seeroo";
+
+    @PostMapping(value = "/user")
+    public String user(@RequestBody UserDetailsDTO userDetailsDTO) {
+        String welcomeMessage = "";
+        if (userDetailsDTO.getLocation().equals(country)) {
+            if (userDetailsDTO.getState().equals(state)) {
+                welcomeMessage = "Namaskaram! " + userDetailsDTO.getName() + ", Welcome to java. ";
+            } else {
+                welcomeMessage = "Namaste " + userDetailsDTO.getName() + " ,Welcome to java. ";
+            }
+            if (userDetailsDTO.getCompany().equals(company)) {
+                welcomeMessage = welcomeMessage + userDetailsDTO.getCompany() + "Welcomes you to the Java Team ";
+            }
+        } else {
+            welcomeMessage = "HOLA! " + userDetailsDTO.getName() + " ,Welcome to java. ";
+        }
+//        return "My name is " + userDetailsDTO.getName() + "\n" +
+//               "I am from " +userDetailsDTO.getLocation() + "\n" +
+//               "I am working at " + userDetailsDTO.getCompany() ;
+        return welcomeMessage;
+
     }
-
-
 }
