72e49ff9ddc25ce99cc76e0852729d9332051137
Pranav <pranav.p@seeroo.com>
2022-04-28 10:50:29 +0530

    [fixed]issue

--- src/main/java/com/seeroo/rentalservice/controller/UserController.java
+++ src/main/java/com/seeroo/rentalservice/controller/UserController.java
@@ -1,37 +1,23 @@
 package com.seeroo.rentalservice.controller;
 
 import com.seeroo.rentalservice.dto.UserDetailsDTO;
+import com.seeroo.rentalservice.service.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.Autowired;
 
 @RestController
 public class UserController {
 
-    public static String country = "India";
-    public static String state = "Kerala";
-    public static String company = "Seeroo";
+    @Autowired private UserServices userServices;
 
     @PostMapping(value = "/user")
     public String user(@RequestBody UserDetailsDTO userDetailsDTO) {
-        String welcomeMessage = "";
-        if (userDetailsDTO.getLocation().equals(country)) {
-            if (userDetailsDTO.getState().equals(state)) {
-                welcomeMessage = "Namaskaram! " + userDetailsDTO.getName() + ", Welcome to java. ";
-            } else {
-                welcomeMessage = "Namaste " + userDetailsDTO.getName() + " ,Welcome to java. ";
-            }
-            if (userDetailsDTO.getCompany().equals(company)) {
-                welcomeMessage = welcomeMessage + userDetailsDTO.getCompany() + "Welcomes you to the Java Team ";
-            }
-        } else {
-            welcomeMessage = "HOLA! " + userDetailsDTO.getName() + " ,Welcome to java. ";
-        }
-//        return "My name is " + userDetailsDTO.getName() + "\n" +
-//               "I am from " +userDetailsDTO.getLocation() + "\n" +
-//               "I am working at " + userDetailsDTO.getCompany() ;
-        return welcomeMessage;
+
+        return userServices.getWelcomeMessage(userDetailsDTO);
 
     }
 }
